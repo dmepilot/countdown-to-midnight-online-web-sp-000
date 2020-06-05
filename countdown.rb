dc4a5afb1c00528f0d1acbf7f35f9bef2c3b53cc
Dean Erickson <dmepilot@yahoo.com>
2020-06-05 02:27:45 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,11 +3,16 @@
 def countdown(count)
   while count > 0 
   puts "#{count} SECOND(S)!"
-  sleep 1 
   count -= 1 
   end
 "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(count)
-  
+  while count > 0 
+  puts "#{count} SECOND(S)!"
+  sleep 1 
+  count -= 1 
+  end
+"HAPPY NEW YEAR!"
+end
